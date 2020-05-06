34485ca37ee48a0acf83eac8bbbf5765375a0cbe
zikrehaimanot <zikrehaimanot@gmail.com>
2020-05-05 20:48:53 -0400

    edit

---
+++ findOddIt.js
@@ -0,0 +1,25 @@
+// Given an array, find the integer that appears an odd number of times.
+//
+// There will always be only one integer that appears an odd number of times.
+//
+//
+
+
+
+function findOdd(arr) {
+  var result, num = 0;
+
+  arr = arr.sort();
+  for (var i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i+1]) {
+      num++;
+    } else {
+      num++;
+      if (num % 2 != 0) {
+        result = arr[i];
+        break;
+      }
+    }
+  }
+  return result;
+}

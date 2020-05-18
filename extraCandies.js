4a79198833ca5fcc85dd6d7b8555d5cf2dfdf4a3
zikrehaimanot <zikrehaimanot@gmail.com>
2020-05-18 15:24:15 -0400

    edit

--- extraCandies.js
+++ extraCandies.js
@@ -3,7 +3,6 @@
 // For each kid check if there is a way to distribute extraCandies among the kids such that he or she can have the greatest number of candies among them. Notice that multiple kids can have the greatest number of candies.
 //
 //
-
 var kidsWithCandies = function(candies, extraCandies) {
     let biggestVal = Math.max(...candies)
     let retur = []
